73f2caf3bfe172c6948acef2791bcdbf4610ebb0
Aleksandr <ovcharenko_aleksandr@ukr.net>
2020-12-17 15:19:15 +0200

    chess

--- js/main.js
+++ js/main.js
@@ -1,41 +1,49 @@
-// let  randomInt = (min, max) => {
-//     let rand = min + Math.random() * (max + 1 - min);
-//     return Math.floor(rand);
-// }
-// console.log(randomInt(2,9));
-// let text = element.textContent;
-// console.log(text);
-document.querySelector('.button').onclick = function(){
-    console.log('GOGOGO');
-    let element = document.querySelector('.input');
-    console.log(typeof(element), element.value, element);
-    let elementValue = element.value;
-    console.log(elementValue);
-    let splitArray = [];
-    splitArray = elementValue.split(" ");
-    console.log(splitArray);
-    let str = '';
-    for(let i = 0; i < splitArray.length; i++) {
-        console.log(i);
-        str = `${str} <textarea class="input" id="${i}"name="text" >${splitArray[i]}</textarea>`;
-    }
-    document.querySelector('.main1').innerHTML = str;
-
-
+let  randomInt = (min, max) => {
+    let rand = min + Math.random() * (max + 1 - min);
+    return Math.floor(rand);
 }
+console.log(randomInt(2,9));
 
-// let splitArray = element.split(" ");
-//     console.log(splitArray);
-    
+const mainEL = document.querySelector('.main');
+console.log(mainEL);
+let str = '';
+for(let i = 0; i < 64 ; i++) {
+    str = str + `<div id="${i}" class="chessboard" > ${i+1} </div> \n`;
+}
+console.log(str);
+mainEL.innerHTML = str;
+const chessBoardEl = document.querySelectorAll('.chessboard');
+console.log(chessBoardEl);
 
 
-// const submitData = () => {
-//     text = text + text;
-//     console.log("text");
-
-// };
-// let str ="After soaking your filter., in a warm water bath for at least five minutes, drop it into the bottom of your siphons top component, or hopper,. and hook to the bottom of the hoppers glass tubing.,Fill your siphon bottom component.,Insert the hopper, filter and all.";
-
-// let splittedArray = str.split(".,");
-
-// console.log(splittedArray);
+for(let i = 0; i < chessBoardEl.length ; i ++) {
+    if (i % 2 === 0 ) {
+        chessBoardEl[i].style.backgroundColor = '#cd851f';
+    } else {
+        chessBoardEl[i].style.backgroundColor ='#ffffb8';
+    }
+}
+chessBoardEl[0].innerHTML = '<img src="./images/br.png" alt=" текст">';
+chessBoardEl[1].innerHTML = '<img src="./images/bn.png" alt=" текст">';
+chessBoardEl[2].innerHTML = '<img src="./images/bb.png" alt=" текст">';
+chessBoardEl[3].innerHTML = '<img src="./images/bk.png" alt=" текст">';
+chessBoardEl[4].innerHTML = '<img src="./images/bq.png" alt=" текст">';
+chessBoardEl[5].innerHTML = '<img src="./images/bb.png" alt=" текст">';
+chessBoardEl[6].innerHTML = '<img src="./images/bn.png" alt=" текст">';
+chessBoardEl[7].innerHTML = '<img src="./images/br.png" alt=" текст">';
+for(i = 8; i < 16; i++) {
+    console.log(i);
+    chessBoardEl[i].innerHTML = '<img src="./images/bp.png" alt=" текст">';
+  };
+  chessBoardEl[56].innerHTML = '<img src="./images/wr.png" alt=" текст">';
+  chessBoardEl[57].innerHTML = '<img src="./images/wn.png" alt=" текст">';
+  chessBoardEl[58].innerHTML = '<img src="./images/wb.png" alt=" текст">';
+  chessBoardEl[59].innerHTML = '<img src="./images/wk.png" alt=" текст">';
+  chessBoardEl[60].innerHTML = '<img src="./images/wq.png" alt=" текст">';
+  chessBoardEl[61].innerHTML = '<img src="./images/wb.png" alt=" текст">';
+  chessBoardEl[62].innerHTML = '<img src="./images/wn.png" alt=" текст">';
+  chessBoardEl[63].innerHTML = '<img src="./images/wr.png" alt=" текст">';
+for(i = 48; i < 56; i++) {
+     
+      chessBoardEl[i].innerHTML = '<img src="./images/wp.png" alt=" текст">';
+    };
