8570c7690bbef783d2af4de50027f9c212737daa
Aleksandr <ovcharenko_aleksandr@ukr.net>
2020-12-26 13:59:42 +0200

    timer

--- js/main.js
+++ js/main.js
@@ -1,49 +1,86 @@
-let  randomInt = (min, max) => {
-    let rand = min + Math.random() * (max + 1 - min);
-    return Math.floor(rand);
-}
-console.log(randomInt(2,9));
+// let  randomInt = (min, max) => {
+//     let rand = min + Math.random() * (max + 1 - min);
+//     return Math.floor(rand);
+// }
+// // console.log(randomInt(2,9));
 
-const mainEL = document.querySelector('.main');
-console.log(mainEL);
-let str = '';
-for(let i = 0; i < 64 ; i++) {
-    str = str + `<div id="${i}" class="chessboard" > ${i+1} </div> \n`;
-}
-console.log(str);
-mainEL.innerHTML = str;
-const chessBoardEl = document.querySelectorAll('.chessboard');
-console.log(chessBoardEl);
+arrow = document.querySelector('.arrow');
+btn = document.querySelector('.button');
+realTime = document.querySelector('.realTime');
+input = document.querySelector('input');
+clock = document.querySelector('.clock');
+hour = document.querySelector('.hour');
+minute = document.querySelector('.minute');
+second = document.querySelector('.second');
+btn.disabled = true;
+let timeNow = Date.now();
+console.log(timeNow);
+realTime.innerHTML = moment().format('HH:mm:ss');
 
 
-for(let i = 0; i < chessBoardEl.length ; i ++) {
-    if (i % 2 === 0 ) {
-        chessBoardEl[i].style.backgroundColor = '#cd851f';
+
+const runTimer = () => {
+const getTime = () => {
+let tempStrHourMin = String(input.value);
+let tempYMonthDay = String(new Date());
+let tempmin = +tempStrHourMin.slice(3, 5);
+let temphour = +tempStrHourMin.slice(0, 2);
+let tempyear = +tempYMonthDay.slice(11, 16);
+let tempday = +tempYMonthDay.slice(8, 10);
+let start = Date.parse(new Date(tempyear, 12, tempday, temphour,tempmin));
+let end = Date.parse(new Date());
+let tempresalt = start - end;
+let thour = Math.floor((tempresalt/ (1000 * 60 *60)) % 24);
+let tminute = Math.floor((tempresalt/ (1000 * 60 )) % 60);
+let tsecond = Math.floor((tempresalt/ 1000 ) % 60);
+hour.innerHTML = thour;
+minute.innerHTML = tminute;
+second.innerHTML = tsecond;
+if (!arrow.classList.contains('anime')) {
+    clearInterval(updateTime);
+} 
+};
+const updateTime = setInterval(getTime, 1000);
+
+
+// clearInterval(updateTime);
+arrow.classList.toggle('anime');
+
+};
+// console.log(input);
+// console.log(btn);
+// console.log(realTime);
+// console.log(clock);
+
+input.addEventListener('input', (event) => {
+    console.log(event.target.value);
+    btn.addEventListener('click', runTimer);
+    if (event.target.value !== "") {
+        btn.disabled = false;
+        // btn.addEventListener('click', runTimer);
     } else {
-        chessBoardEl[i].style.backgroundColor ='#ffffb8';
+        btn.disabled = true;
+        // btn.removeEventListener('click', runTimer);
+        // btn.addEventListener('click', runTimer);
     }
-}
-chessBoardEl[0].innerHTML = '<img src="./images/br.png" alt=" текст">';
-chessBoardEl[1].innerHTML = '<img src="./images/bn.png" alt=" текст">';
-chessBoardEl[2].innerHTML = '<img src="./images/bb.png" alt=" текст">';
-chessBoardEl[3].innerHTML = '<img src="./images/bk.png" alt=" текст">';
-chessBoardEl[4].innerHTML = '<img src="./images/bq.png" alt=" текст">';
-chessBoardEl[5].innerHTML = '<img src="./images/bb.png" alt=" текст">';
-chessBoardEl[6].innerHTML = '<img src="./images/bn.png" alt=" текст">';
-chessBoardEl[7].innerHTML = '<img src="./images/br.png" alt=" текст">';
-for(i = 8; i < 16; i++) {
-    console.log(i);
-    chessBoardEl[i].innerHTML = '<img src="./images/bp.png" alt=" текст">';
-  };
-  chessBoardEl[56].innerHTML = '<img src="./images/wr.png" alt=" текст">';
-  chessBoardEl[57].innerHTML = '<img src="./images/wn.png" alt=" текст">';
-  chessBoardEl[58].innerHTML = '<img src="./images/wb.png" alt=" текст">';
-  chessBoardEl[59].innerHTML = '<img src="./images/wk.png" alt=" текст">';
-  chessBoardEl[60].innerHTML = '<img src="./images/wq.png" alt=" текст">';
-  chessBoardEl[61].innerHTML = '<img src="./images/wb.png" alt=" текст">';
-  chessBoardEl[62].innerHTML = '<img src="./images/wn.png" alt=" текст">';
-  chessBoardEl[63].innerHTML = '<img src="./images/wr.png" alt=" текст">';
-for(i = 48; i < 56; i++) {
-     
-      chessBoardEl[i].innerHTML = '<img src="./images/wp.png" alt=" текст">';
-    };
+});
+
+let newInMs = new Date();
+console.log(newInMs);
+// console.log(now.getTime());
+
+// let timeInMs = Date.now();
+// console.log(timeInMs);
+
+
+// const start = now.getTime();
+// const end = getTime(event.target.value);
+
+// console.log(start, end);
+// // 
+// console.log();
+// let start = moment().format('HH:mm:ss');
+// console.log(start);
+// let end   = moment().format('HH:mm');
+// // console.log(end.to(start));  
+
